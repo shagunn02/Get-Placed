d8f643a46e7b0b3b769aff9ff222f0ed93f33be8
Shagun jain <greatshagun2005@gmail.com>
2025-10-26 22:31:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://get-placed-9rth.onrender.com',
   credentials: true
 };
 app.use(cors(corsOptions));
