664266870304b1334bdb1c561d819fbf4d3059ae
Shagun jain <greatshagun2005@gmail.com>
2025-10-29 18:59:10 +0530

    Update Login.jsx

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
         e.preventDefault();
         try {
             dispatch(setLoading(true));
-            const res = await axios.post("https://get-placed-9rth.onrender.com/api/v1/user/login", input, {
+            const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
                 headers: {
                     "Content-Type": "application/json"
                 },
