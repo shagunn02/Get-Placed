162691ade0000a30baa1b51c9443fce41daeb5e1
Shagun jain <greatshagun2005@gmail.com>
2025-10-29 18:57:50 +0530

    Update CompanyCreate.jsx

--- frontend/src/components/admin/CompanyCreate.jsx
+++ frontend/src/components/admin/CompanyCreate.jsx
@@ -1,145 +1,60 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Navbar from '../shared/Navbar'
-import { Button } from '../ui/button'
-import { ArrowLeft, Loader2 } from 'lucide-react'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { COMPANY_API_END_POINT } from '@/utils/constant'
-import { useNavigate, useParams } from 'react-router-dom'
 import { toast } from 'sonner'
-import { useSelector } from 'react-redux'
-import useGetCompanyById from '@/hooks/useGetCompanyById'
+import { useDispatch } from 'react-redux'
+import { setSingleCompany } from '@/redux/companySlice'
 
-const CompanySetup = () => {
-    const params = useParams();
-    useGetCompanyById(params.id);
-    const [input, setInput] = useState({
-        name: "",
-        description: "",
-        website: "",
-        location: "",
-        file: null
-    });
-    const {singleCompany} = useSelector(store=>store.company);
-    const [loading, setLoading] = useState(false);
+const CompanyCreate = () => {
     const navigate = useNavigate();
-
-    const changeEventHandler = (e) => {
-        setInput({ ...input, [e.target.name]: e.target.value });
-    }
-
-    const changeFileHandler = (e) => {
-        const file = e.target.files?.[0];
-        setInput({ ...input, file });
-    }
-
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append("name", input.name);
-        formData.append("description", input.description);
-        formData.append("website", input.website);
-        formData.append("location", input.location);
-        if (input.file) {
-            formData.append("file", input.file);
-        }
+    const [companyName, setCompanyName] = useState();
+    const dispatch = useDispatch();
+    const registerNewCompany = async () => {
         try {
-            setLoading(true);
-            const res = await axios.put(`${COMPANY_API_END_POINT}/update/${params.id}`, formData, {
+            const res = await axios.post("/register", { companyName }, {
                 headers: {
-                    'Content-Type': 'multipart/form-data'
+                    'Content-Type': 'application/json'
                 },
                 withCredentials: true
             });
-            if (res.data.success) {
+            if (res?.data?.success) {
+                dispatch(setSingleCompany(res.data.company));
                 toast.success(res.data.message);
-                navigate("/admin/companies");
+                const companyId = res?.data?.company?._id;
+                navigate(`/admin/companies/${companyId}`);
             }
         } catch (error) {
             console.log(error);
-            toast.error(error.response.data.message);
-        } finally {
-            setLoading(false);
         }
     }
-
-    useEffect(() => {
-        setInput({
-            name: singleCompany.name || "",
-            description: singleCompany.description || "",
-            website: singleCompany.website || "",
-            location: singleCompany.location || "",
-            file: singleCompany.file || null
-        })
-    },[singleCompany]);
-
     return (
         <div>
             <Navbar />
-            <div className='max-w-xl mx-auto my-10'>
-                <form onSubmit={submitHandler}>
-                    <div className='flex items-center gap-5 p-8'>
-                        <Button onClick={() => navigate("/admin/companies")} variant="outline" className="flex items-center gap-2 text-gray-500 font-semibold">
-                            <ArrowLeft />
-                            <span>Back</span>
-                        </Button>
-                        <h1 className='font-bold text-xl'>Company Setup</h1>
-                    </div>
-                    <div className='grid grid-cols-2 gap-4'>
-                        <div>
-                            <Label>Company Name</Label>
-                            <Input
-                                type="text"
-                                name="name"
-                                value={input.name}
-                                onChange={changeEventHandler}
-                            />
-                        </div>
-                        <div>
-                            <Label>Description</Label>
-                            <Input
-                                type="text"
-                                name="description"
-                                value={input.description}
-                                onChange={changeEventHandler}
-                            />
-                        </div>
-                        <div>
-                            <Label>Website</Label>
-                            <Input
-                                type="text"
-                                name="website"
-                                value={input.website}
-                                onChange={changeEventHandler}
-                            />
-                        </div>
-                        <div>
-                            <Label>Location</Label>
-                            <Input
-                                type="text"
-                                name="location"
-                                value={input.location}
-                                onChange={changeEventHandler}
-                            />
-                        </div>
-                        <div>
-                            <Label>Logo</Label>
-                            <Input
-                                type="file"
-                                accept="image/*"
-                                onChange={changeFileHandler}
-                            />
-                        </div>
-                    </div>
-                    {
-                        loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> : <Button type="submit" className="w-full my-4">Update</Button>
-                    }
-                </form>
-            </div>
+            <div className='max-w-4xl mx-auto'>
+                <div className='my-10'>
+                    <h1 className='font-bold text-2xl'>Your Company Name</h1>
+                    <p className='text-gray-500'>What would you like to give your company name? you can change this later.</p>
+                </div>
 
+                <Label>Company Name</Label>
+                <Input
+                    type="text"
+                    className="my-2"
+                    placeholder="JobHunt, Microsoft etc."
+                    onChange={(e) => setCompanyName(e.target.value)}
+                />
+                <div className='flex items-center gap-2 my-10'>
+                    <Button variant="outline" onClick={() => navigate("/admin/companies")}>Cancel</Button>
+                    <Button onClick={registerNewCompany}>Continue</Button>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default CompanySetup
+export default CompanyCreate
