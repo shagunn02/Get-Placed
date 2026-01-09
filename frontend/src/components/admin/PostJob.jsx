ef8a3b7b882e03bd0ae4887d17c859a0b3ec18aa
Shagun jain <greatshagun2005@gmail.com>
2026-01-09 13:36:01 +0530

    Update PostJob.jsx

--- frontend/src/components/admin/PostJob.jsx
+++ frontend/src/components/admin/PostJob.jsx
@@ -42,7 +42,7 @@ const PostJob = () => {
         e.preventDefault();
         try {
             setLoading(true);
-            const res = await axios.post(`${JOB_API_END_POINT}/create`, input,{
+            const res = await axios.post(`${JOB_API_END_POINT}/post`, input,{
                 headers:{
                     'Content-Type':'application/json'
                 },
