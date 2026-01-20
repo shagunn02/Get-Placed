355f55bbfe89347b5f879348f208f1820c984a24
Shagun jain <greatshagun2005@gmail.com>
2026-01-20 10:11:34 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.use(cors(corsOptions));
 // routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/jobs", jobRoute);
+app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 app.use(express.static(path.join(_dirname, "/frontend/dist")))
